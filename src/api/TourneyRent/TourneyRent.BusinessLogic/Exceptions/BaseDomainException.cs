4ceb496ee1adb4678fb5452d048736f3155d5720
jusrus01 <jjustinas.ruslys@gmail.com>
2023-02-20 19:20:10 +0200

    Add comment

--- src/api/TourneyRent/TourneyRent.BusinessLogic/Exceptions/BaseDomainException.cs
+++ src/api/TourneyRent/TourneyRent.BusinessLogic/Exceptions/BaseDomainException.cs
@@ -1,5 +1,11 @@
 ﻿namespace TourneyRent.BusinessLogic.Exceptions
 {
+    /// <summary>
+    /// Other exceptions should inherit from this exception.
+    /// Those exceptions should be thrown in the service layer since
+    /// GlobalExceptionMiddleware will catch them and return correct
+    /// error response for the front-end to handle.
+    /// </summary>
     public abstract class BaseDomainException : Exception
     {
         public BaseDomainException(string errorMessage, Exception inner)
