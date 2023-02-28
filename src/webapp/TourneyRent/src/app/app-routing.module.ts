798296c87869f9034e0ac15f6e4cb28c1edb7f5e
matas01 <99809130+matas01@users.noreply.github.com>
2023-02-28 19:58:45 +0200

    /rental-create front end

--- src/webapp/TourneyRent/src/app/app-routing.module.ts
+++ src/webapp/TourneyRent/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AuthGuard } from './guard/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { RentalCreateComponent } from './pages/rental-create/rental-create.component';
 
 export const ROUTES: Routes = [
   { path: 'login', component: LoginComponent, data: { title: 'Login' } },
   { path: 'register', component: RegisterComponent, data: { title: 'Register' } },
+  { path: 'rental-create', component: RentalCreateComponent, data: { title: 'Rental Create' } },
   { path: '', component: HomeComponent, data: { title: 'Home' }, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
 ];
