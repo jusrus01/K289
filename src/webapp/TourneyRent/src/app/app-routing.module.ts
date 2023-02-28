c1060e287b7abbcb6bc46925f6516cf089ae7b36
T1t45 <titasbartulis@gmail.com>
2023-02-28 22:44:27 +0200

    made a creation of a team page

--- src/webapp/TourneyRent/src/app/app-routing.module.ts
+++ src/webapp/TourneyRent/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AuthGuard } from './guard/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { TeamCreateComponent } from './pages/team-create/team-create/team-create.component';
 
 export const ROUTES: Routes = [
   { path: 'login', component: LoginComponent, data: { title: 'Login' } },
   { path: 'register', component: RegisterComponent, data: { title: 'Register' } },
+  { path: 'team-create', component: TeamCreateComponent, data: {title: 'Team Create' } },
   { path: '', component: HomeComponent, data: { title: 'Home' }, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
 ];
