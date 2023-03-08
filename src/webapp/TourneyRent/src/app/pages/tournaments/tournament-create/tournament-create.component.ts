44df43a6639be0e8974064ca540be05906f31ffb
jusrus01 <jjustinas.ruslys@gmail.com>
2023-03-08 20:59:16 +0200

    Add image upload

--- src/webapp/TourneyRent/src/app/pages/tournaments/tournament-create/tournament-create.component.ts
+++ src/webapp/TourneyRent/src/app/pages/tournaments/tournament-create/tournament-create.component.ts
@@ -7,8 +7,22 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./tournament-create.component.scss']
 })
 export class TournamentCreateComponent {
+  public pictureSource!: string | ArrayBuffer | null;
+  public pictureFile!: any;
+
   range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
+
+  onFileUpload(event: any, upload: any): void {
+    this.pictureFile = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = () => (this.pictureSource = reader.result);
+    reader.readAsDataURL(this.pictureFile);
+    upload.value = null;
+    const formData = new FormData();
+    formData.set('imageFile', this.pictureFile);
+  }
 }
