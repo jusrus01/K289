39188ee1d5a1817155a1dc75095eddf96c34668d
T1t45 <titasbartulis@gmail.com>
2023-03-30 14:10:02 +0300

    trying to display created teams
    .

--- src/webapp/TourneyRent/src/app/pages/teams/team-list/team-list.component.ts
+++ src/webapp/TourneyRent/src/app/pages/teams/team-list/team-list.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TeamResource } from 'src/app/resources/team.resource';
 import { RoutingService } from 'src/app/services/routing.service';
+
+export interface Team {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface Member {
+  id: number;
+  name: string;
+  email: string;
+  teamId: number;
+}
+
+export interface TeamWithMembers {
+  team: Team;
+  members: Member[];
+}
 
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
   styleUrls: ['./team-list.component.scss']
 })
-export class TeamListComponent {
+export class TeamListComponent implements OnInit {
+  public teams: Team[] = [];
+  public teamsWithMembers: TeamWithMembers[] = [];
+
     constructor(
+      public teamResource: TeamResource,
       public routing: RoutingService
     ) {}
+
+    ngOnInit() {
+      this.teamResource.getAllTeams().subscribe((teams: Team[]) => {
+        this.teams = teams;
+        for (const team of this.teams) {
+          this.teamResource.getTeamMembers(team.id).subscribe((members: Member[]) => {
+            this.teamsWithMembers.push({ team, members });
+          });
+        }
+      });
+    }
+    
 }
