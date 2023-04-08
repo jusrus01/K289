082c0634b046593d1d437868757249890ce8f49e
T1t45 <titasbartulis@gmail.com>
2023-04-09 00:13:07 +0300

    Can see all the teams that I have joined in my profile page

--- src/webapp/TourneyRent/src/app/pages/profile/profile.component.ts
+++ src/webapp/TourneyRent/src/app/pages/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { ImageComponent } from 'src/app/common/image/image.component';
 import { Profile } from 'src/app/models/profiles/profile.model';
 import { ProfileResource } from 'src/app/resources/profile.resource';
 import { TournamentResource } from 'src/app/resources/tournament.resource';
+import { TeamResource } from 'src/app/resources/team.resource';
 import { AuthService } from 'src/app/services/auth.service';
 import { TournamentService } from 'src/app/services/tournament.service';
 
@@ -28,11 +29,13 @@ export class ProfileComponent {
   public pictureFile!: any;
 
   public tournaments: any;
+  public teams: any;
 
   constructor(
     private profileResource: ProfileResource,
     private authService: AuthService,
     private tournamentResource: TournamentResource,
+    private teamResource: TeamResource,
     private tournamentService: TournamentService
   ) {
     this.isLoading = true;
@@ -56,6 +59,14 @@ export class ProfileComponent {
           status: this.tournamentService.getTournamentStatus(t)
         }));
       })
+
+    this.teamResource
+    .getUserTeams(this.authService.getAuthUserId())
+    .subscribe(teams => {
+      this.teams = teams.map((t: any) => ({
+        ...t
+      }));
+    })
   }
 
 
