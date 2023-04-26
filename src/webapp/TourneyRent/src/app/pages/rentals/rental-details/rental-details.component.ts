fe3f72fc5c68cd4f9c54184ac53aac6e0fc0e516
matas01 <99809130+matas01@users.noreply.github.com>
2023-04-26 18:07:31 +0300

    rental update
    new design + highlight

--- src/webapp/TourneyRent/src/app/pages/rentals/rental-details/rental-details.component.ts
+++ src/webapp/TourneyRent/src/app/pages/rentals/rental-details/rental-details.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RentalResource } from 'src/app/resources/rental.resource';
 import { RoutingService } from 'src/app/services/routing.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {ThemePalette} from '@angular/material/core';
+import {ProgressSpinnerMode} from '@angular/material/progress-spinner';
+
 
 @Component({
   selector: 'app-rental-details',
@@ -11,7 +15,7 @@ import { RoutingService } from 'src/app/services/routing.service';
 export class RentalDetailsComponent implements OnInit {
   item: any;
 
-  constructor(private dataService: RentalResource, private route: ActivatedRoute, public routing: RoutingService) { }
+  constructor(public dialog: MatDialog, private dataService: RentalResource, private route: ActivatedRoute, public routing: RoutingService) { }
 
   ngOnInit(): void {
     const itemId = Number(this.route.snapshot.paramMap.get('id'));
@@ -19,4 +23,73 @@ export class RentalDetailsComponent implements OnInit {
       this.item = data;
     });
   }
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '400px',
+      height: '200px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 }
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: './dialog-animations-example-dialog.html',
+})
+export class DialogAnimationsExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>) {}
+}
+
+@Component({
+  selector: 'progress-spinner-configurable-example',
+  templateUrl: 'progress-spinner-configurable-example.html',
+  styleUrls: ['progress-spinner-configurable-example.css'],
+})
+export class ProgressSpinnerConfigurableExample {
+}
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { RentalResource } from 'src/app/resources/rental.resource';
+// import { RoutingService } from 'src/app/services/routing.service';
+
+// @Component({
+//   selector: 'app-rental-details',
+//   templateUrl: './rental-details.component.html',
+//   styleUrls: ['./rental-details.component.scss']
+// })
+// export class RentalDetailsComponent implements OnInit {
+//   item: any;
+//   showDialog = false;
+//   showSuccess = false;
+
+//   constructor(private dataService: RentalResource, private route: ActivatedRoute, public routing: RoutingService) { }
+
+//   ngOnInit(): void {
+//     const itemId = Number(this.route.snapshot.paramMap.get('id'));
+//     this.dataService.getItemById(itemId).subscribe((data: any) => {
+//       this.item = data;
+//     });
+//   }
+
+//   pay(): void {
+//     this.showDialog = true;
+//   }
+
+//   confirmPayment(): void {
+//     this.showDialog = false;
+//     this.showSuccess = true;
+//     setTimeout(() => {
+//       this.showSuccess = false;
+//     }, 2000);
+//     setTimeout(() => {
+//       // Perform the transaction here
+//     }, 4000);
+//   }
+// }
+
+
+
