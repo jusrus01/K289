ff6a830832569d9bcbb8da56c7c16fa97305e1b2
TadisOn <76095383+TadisOn@users.noreply.github.com>
2023-04-27 16:36:01 +0300

    Fixed issues with DB
    - Update your branches ASAP
    - After Update execute 'Update-Database'

--- src/api/TourneyRent/TourneyRent.DataLayer/Migrations/20230427133112_AddPrize.cs
+++ src/api/TourneyRent/TourneyRent.DataLayer/Migrations/20230427133112_AddPrize.cs
@@ -98,7 +98,7 @@ namespace TourneyRent.DataLayer.Migrations
                 oldType: "nvarchar(450)");
 
             migrationBuilder.AlterColumn<string>(
-                name: "Image",
+                name: "ImageId",
                 table: "RentalItems",
                 type: "nvarchar(max)",
                 nullable: true,
@@ -138,7 +138,7 @@ namespace TourneyRent.DataLayer.Migrations
                 oldType: "nvarchar(max)");
 
             migrationBuilder.CreateTable(
-                name: "Prize",
+                name: "Prizes",
                 columns: table => new
                 {
                     Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
@@ -159,7 +159,7 @@ namespace TourneyRent.DataLayer.Migrations
 
             migrationBuilder.CreateIndex(
                 name: "IX_Prize_TournamentId",
-                table: "Prize",
+                table: "Prizes",
                 column: "TournamentId");
 
             migrationBuilder.AddForeignKey(
@@ -203,6 +203,90 @@ namespace TourneyRent.DataLayer.Migrations
                 column: "UserId",
                 principalTable: "AspNetUsers",
                 principalColumn: "Id");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prize_Tournaments_TournamentId",
+                table: "Prizes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Prize",
+                table: "Prizes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Prize_TournamentId",
+                table: "Prizes",
+                newName: "IX_Prizes_TournamentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Prizes",
+                table: "Prizes",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("16157e63-9308-4a3b-8493-f8b030886da5"), "Gaming monitor", null, "KOORUI 24.5 Inch FHD Gaming Monitor (used)", null });
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("28d718d3-440f-4110-8af6-a5fa5cdbeb17"), "Laptop", null, "Dell Latitude 3520 Laptop 15.6 (used)", null });
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("a4642138-fdd9-41db-b408-80cb5a3f7c5b"), "Gaming monitor", null, "DELL Latitude 5490 (used)", null });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prizes_Tournaments_TournamentId",
+                table: "Prizes",
+                column: "TournamentId",
+                principalTable: "Tournaments",
+                principalColumn: "Id");
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("16157e63-9308-4a3b-8493-f8b030886da5"));
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("28d718d3-440f-4110-8af6-a5fa5cdbeb17"));
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("a4642138-fdd9-41db-b408-80cb5a3f7c5b"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWinnerSelected",
+                table: "Tournaments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWinner",
+                table: "Participants",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("239a3846-3852-4e89-99e9-f4d7f066460d"), "Gaming monitor", null, "KOORUI 24.5 Inch FHD Gaming Monitor (used)", null });
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("57cb55f2-ec7a-4a47-862e-f08da07fccd7"), "Gaming monitor", null, "DELL Latitude 5490 (used)", null });
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("f192bf13-861f-4e50-a406-5ffcbeb7278a"), "Laptop", null, "Dell Latitude 3520 Laptop 15.6 (used)", null });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -232,7 +316,7 @@ namespace TourneyRent.DataLayer.Migrations
                 table: "Transactions");
 
             migrationBuilder.DropTable(
-                name: "Prize");
+                name: "Prizes");
 
             migrationBuilder.AlterColumn<string>(
                 name: "UserId",
@@ -315,7 +399,7 @@ namespace TourneyRent.DataLayer.Migrations
                 oldNullable: true);
 
             migrationBuilder.AlterColumn<string>(
-                name: "Image",
+                name: "ImageId",
                 table: "RentalItems",
                 type: "nvarchar(max)",
                 nullable: false,
@@ -411,6 +495,84 @@ namespace TourneyRent.DataLayer.Migrations
                 principalTable: "AspNetUsers",
                 principalColumn: "Id",
                 onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prizes_Tournaments_TournamentId",
+                table: "Prizes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Prizes",
+                table: "Prizes");
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("16157e63-9308-4a3b-8493-f8b030886da5"));
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("28d718d3-440f-4110-8af6-a5fa5cdbeb17"));
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("a4642138-fdd9-41db-b408-80cb5a3f7c5b"));
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Prizes_TournamentId",
+                table: "Prizes",
+                newName: "IX_Prize_TournamentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Prize",
+                table: "Prizes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prize_Tournaments_TournamentId",
+                table: "Prizes",
+                column: "TournamentId",
+                principalTable: "Tournaments",
+                principalColumn: "Id");
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("239a3846-3852-4e89-99e9-f4d7f066460d"));
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("57cb55f2-ec7a-4a47-862e-f08da07fccd7"));
+
+            migrationBuilder.DeleteData(
+                table: "Prizes",
+                keyColumn: "Id",
+                keyValue: new Guid("f192bf13-861f-4e50-a406-5ffcbeb7278a"));
+
+            migrationBuilder.DropColumn(
+                name: "IsWinnerSelected",
+                table: "Tournaments");
+
+            migrationBuilder.DropColumn(
+                name: "IsWinner",
+                table: "Participants");
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("16157e63-9308-4a3b-8493-f8b030886da5"), "Gaming monitor", null, "KOORUI 24.5 Inch FHD Gaming Monitor (used)", null });
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("28d718d3-440f-4110-8af6-a5fa5cdbeb17"), "Laptop", null, "Dell Latitude 3520 Laptop 15.6 (used)", null });
+
+            migrationBuilder.InsertData(
+                table: "Prizes",
+                columns: new[] { "Id", "Description", "ImageId", "Name", "TournamentId" },
+                values: new object[] { new Guid("a4642138-fdd9-41db-b408-80cb5a3f7c5b"), "Gaming monitor", null, "DELL Latitude 5490 (used)", null });
         }
     }
 }
