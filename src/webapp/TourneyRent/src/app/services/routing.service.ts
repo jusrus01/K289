b820bf7a3f95797d34830cacda56a74221dd99d1
jusrus01 <jjustinas.ruslys@gmail.com>
2023-04-27 23:05:31 +0300

    test workdlow

--- src/webapp/TourneyRent/src/app/services/routing.service.ts
+++ src/webapp/TourneyRent/src/app/services/routing.service.ts
@@ -1,24 +1,5 @@
 import { Inject, Injectable, Type } from '@angular/core';
-import { Route, Router, ROUTES } from '@angular/router';
-import { HomeComponent } from '../pages/home/home.component';
-import { LoginComponent } from '../pages/login/login.component';
-import { ProfileComponent } from '../pages/profile/profile.component';
-import { RegisterComponent } from '../pages/register/register.component';
-import { RentalViewComponent } from '../pages/rentals/rental-view/rental-view.component';
-import { TeamCreateComponent } from '../pages/teams/team-create/team-create.component';
-import { TeamComponent } from '../pages/teams/team/team.component';
-import { TournamentCreateComponent } from '../pages/tournaments/tournament-create/tournament-create.component';
-import { TournamentUpdateComponent } from '../pages/tournaments/tournament-update/tournament-update.component';
-import { TournamentComponent } from '../pages/tournaments/tournament/tournament.component';
-import { RentalComponent } from '../pages/rentals/rental/rental.component';
-import { RentalCreateComponent } from '../pages/rentals/rental-create/rental-create.component';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class RoutingService {
-  constructor(@Inject(ROUTES) private _routes: Route[][], private _router: Router) {}
-  
+=
   public goToRentalView(): void {
     this.navigateToComponent(RentalViewComponent);
   }
